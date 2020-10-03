799ea8387a1c2b43f9f4b43535140ef0c8e17fd5
Osiris <potterosiris@gmail.com>
2020-10-03 17:14:06 -0400

    blog item . css / blog.js component

--- src/blog/pages/Blog.js
+++ src/blog/pages/Blog.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import BlogList from "../components/BlogList";
 
 const Blog = () => {
-  return <div>hi</div>;
+  const BLOG = [
+    {
+      id: "be1",
+      blgimg: "https://en.wikipedia.org/wiki/Ra#/media/File:Ra_Barque.jpg",
+      blgentry: "this is my first blog post",
+      creator: "ra",
+    },
+    {
+      id: "be1",
+      blgimg:
+        "https://en.wikipedia.org/wiki/Osiris#/media/File:The_judgement_of_the_dead_in_the_presence_of_Osiris.jpg",
+      blgentry: "this is basically my first post",
+      creator: "osiris",
+    },
+    {
+      id: "be1",
+      blgimg:
+        "https://en.wikipedia.org/wiki/Set_(deity)#/media/File:Set_speared_Apep.jpg",
+      blgentry: "this is me seth's first entry",
+      creator: "seth",
+    },
+  ];
+
+  const userId = useParams().userId;
+  const loadedEntrys = BLOG.filter((entry) => entry.creator === userId);
+  return <BlogList items={loadedEntrys} />;
 };
 
 export default Blog;
