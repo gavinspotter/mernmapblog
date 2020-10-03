df19592ae19fe2c4f7bc14348d02011f8671c210
Osiris <potterosiris@gmail.com>
2020-10-03 15:37:15 -0400

    blog list . js component

--- src/blog/components/BlogList.js
+++ src/blog/components/BlogList.js
@@ -1,5 +1,27 @@
 import React from "react";
+import BlogItem from "./BlogItem";
 
-const BlogList = () => {};
+const BlogList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div>
+        <h2>no blog posts found</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul>
+      {props.items.map((blg) => (
+        <BlogItem
+          key={blg.id}
+          id={blg.id}
+          img={blg.blgimg}
+          blog={blg.blgentry}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default BlogList;
