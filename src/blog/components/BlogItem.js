6e309c8a6df86faf3529df66824f3b80456f5cc0
Osiris <potterosiris@gmail.com>
2020-10-03 15:28:32 -0400

    blogitem.js component

--- src/blog/components/BlogItem.js
+++ src/blog/components/BlogItem.js
@@ -1,5 +1,23 @@
 import React from "react";
 
-const BlogItem = () => {};
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+
+const BlogItem = (props) => {
+  return (
+    <li>
+      <Card>
+        <div>
+          <div>
+            <img src={props.img} alt={props.title} />
+          </div>
+          <div>{props.blog}</div>
+        </div>
+        <Button>edit</Button>
+        <Button>delete</Button>
+      </Card>
+    </li>
+  );
+};
 
 export default BlogItem;
