d3881a5602f7015d32deed36140caf222c737601
Osiris <potterosiris@gmail.com>
2020-10-04 20:50:24 -0400

    updateentry.js for journal w dummy date use params and first  input

--- src/journal/pages/UpdateEntry.js
+++ src/journal/pages/UpdateEntry.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import Input from "../../shared/components/FormElements/Input";
+
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+const JOURNAL = [
+  {
+    id: "j1",
+    date: "aug 25",
+    entry: "my first journal entry",
+    creator: "u1",
+  },
+  {
+    id: "j1",
+    date: "aug 25",
+    entry: "my first journal entry",
+    creator: "u2",
+  },
+  {
+    id: "j2",
+    date: "aug 25",
+    entry: "my second journal entry",
+    creator: "u1",
+  },
+];
 
 const UpdateEntry = () => {
-  return <h2> update entry </h2>;
+  const journalId = useParams().journalId;
+  const identifiedJournal = JOURNAL.find((j) => j.id === journalId);
+
+  if (!identifiedJournal) {
+    return (
+      <div>
+        <h2> could not find place </h2>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        id="date"
+        element="input"
+        type="text"
+        label="date"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="enter valid date"
+        onInput={() => {}}
+        value={identifiedJournal.date}
+        valid={true}
+      />
+    </form>
+  );
 };
 
 export default UpdateEntry;
