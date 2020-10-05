1d60c0423c5f45bbb39713930d6996fe0444fe76
Osiris <potterosiris@gmail.com>
2020-10-04 21:58:21 -0400

    update entry button

--- src/journal/pages/UpdateEntry.js
+++ src/journal/pages/UpdateEntry.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Input from "../../shared/components/FormElements/Input";
-
+import Button from "../../shared/components/FormElements/Button";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
@@ -62,6 +62,9 @@ const UpdateEntry = () => {
         value={identifiedJournal.date}
         valid={true}
       />
+      <Button type="submit" disabled={true}>
+        update journal{" "}
+      </Button>
     </form>
   );
 };
