4f49efb1a849563a1d16dcfad213c46a48852db2
Osiris <potterosiris@gmail.com>
2020-10-10 21:35:00 -0400

    use form hook

--- src/blog/pages/NewBlog.js
+++ src/blog/pages/NewBlog.js
@@ -1,12 +1,20 @@
 import React from "react";
 import FormHook from "../../shared/components/FormElements/FormHook";
 import "./NewBlog.css";
-
+import Card from "../../shared/components/UIElements/Card";
+import FormInput from "../../shared/components/FormElements/FormInput";
+import Button from "../../shared/components/FormElements/Button";
+import { useForm } from "react-hook-form";
 const NewBlog = () => {
+  const { register, handleSubmit } = useForm();
   return (
-    <div className="blog-form">
-      <FormHook />
-    </div>
+    <Card>
+      <form>
+        <FormInput element="input" element="input" nam1="hi" />
+        <FormInput />
+        <Button type="submit"></Button>
+      </form>
+    </Card>
   );
 };
 
