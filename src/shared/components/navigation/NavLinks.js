85078dde2432582577b01f6620dcdcd4bff974dd
Osiris <potterosiris@gmail.com>
2020-10-17 15:47:31 -0400

    navlinks context

--- src/shared/components/navigation/NavLinks.js
+++ src/shared/components/navigation/NavLinks.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+
+import { AuthContext } from "../../context/auth-context";
 
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
@@ -11,25 +15,36 @@ const NavLinks = (props) => {
           ALL USERS
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">MY PLACES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">ADD PLACE</NavLink>
-      </li>
-
-      <li>
-        <NavLink to="/u1/journal">MY JOURNAL ENTRYS</NavLink>
-      </li>
-      <li>
-        <NavLink to="/journal/new">ADD JOURNAL ENTRY</NavLink>
-      </li>
-      <li>
-        <NavLink to="/ra/blog">MY BLOG</NavLink>
-      </li>
-      <li>
-        <NavLink to="/blog/new">ADD BLOG POST</NavLink>
-      </li>
+      {auth.isLoggedin && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {auth.isLoggedin && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {auth.isLoggedin && (
+        <li>
+          <NavLink to="/u1/journal">MY JOURNAL ENTRYS</NavLink>
+        </li>
+      )}
+      {auth.isLoggedin && (
+        <li>
+          <NavLink to="/journal/new">ADD JOURNAL ENTRY</NavLink>
+        </li>
+      )}
+      {auth.isLoggedin && (
+        <li>
+          <NavLink to="/ra/blog">MY BLOG</NavLink>
+        </li>
+      )}
+      {auth.isLoggedin && (
+        <li>
+          <NavLink to="/blog/new">ADD BLOG POST</NavLink>
+        </li>
+      )}
       <li>
         <NavLink to="/auth">AUTHENTICATE</NavLink>
       </li>
