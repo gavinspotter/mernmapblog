d56524eb9dfdb73cca6c9d784a187852a40e3d13
Osiris <potterosiris@gmail.com>
2020-11-17 18:24:54 -0500

    working on http-hook

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -1,25 +1,33 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const sendRequest = async (
-    url,
-    method = "GET",
-    body = null,
-    headers = {}
-  ) => {
-    const response = await fetch(url, {
-      method,
-      body,
-      headers,
-    });
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+        });
 
-    const responseData = await response.json();
+        const responseData = await response.json();
 
-    if (!response.ok) {
-      throw new Error(responseData.message);
-    }
-  };
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    },
+    []
+  );
+
+  return { isLoading, error, sendRequest };
 };
