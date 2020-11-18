065259193fcc9044f82fc6f291fe362511ce68e2
Osiris <potterosiris@gmail.com>
2020-11-17 19:07:52 -0500

    authjs refactored w http-hook

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -26,11 +26,13 @@ export const useHttpClient = () => {
           throw new Error(responseData.message);
         }
 
+        setIsLoading(false);
         return responseData;
       } catch (err) {
         setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-      setIsLoading(false);
     },
     []
   );
