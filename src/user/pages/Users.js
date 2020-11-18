fb4c8af02e4f71de9ca8c42dd4ee5d6a7e407733
Osiris <potterosiris@gmail.com>
2020-11-17 19:26:59 -0500

    import http hook

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,6 +4,7 @@ import UsersList from "../components/UsersList";
 
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const Users = () => {
   const [isLoading, setIsLoading] = useState(false);
