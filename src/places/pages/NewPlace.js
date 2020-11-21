af90eb2f6ebffb8c037d3dd02b354681e39fdbd9
Osiris <potterosiris@gmail.com>
2020-11-21 14:32:34 -0500

    react frag

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -53,36 +53,38 @@ const NewPlace = () => {
   };
 
   return (
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="please enter a valid title"
-        onInput={inputHandler}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="please enter a valid description (5 characters)"
-        onInput={inputHandler}
-      />
-      <Input
-        id="address"
-        element="input"
-        label="address"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="please enter a valid address"
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        add place
-      </Button>
-    </form>
+    <React.Fragment>
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="please enter a valid title"
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="please enter a valid description (5 characters)"
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          label="address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="please enter a valid address"
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          add place
+        </Button>
+      </form>
+    </React.Fragment>
   );
 };
 
