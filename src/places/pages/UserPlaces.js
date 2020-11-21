dbc6257a654a089a82c5f7c9da8a96f38e93d5fc
Osiris <potterosiris@gmail.com>
2020-11-21 18:27:11 -0500

    use effect hook

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
@@ -6,7 +6,11 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const UserPlaces = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const userId = useParams().userId;
+
+  useEffect(() => {}, [sendRequest]);
+
   const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
   return <PlaceList items={loadedPlaces} />;
 };
