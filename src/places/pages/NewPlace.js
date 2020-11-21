cd11136ba583d86f8f898f8d74c5f515235638aa
Osiris <potterosiris@gmail.com>
2020-11-21 14:28:54 -0500

    use context const

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -11,8 +11,10 @@ import { useForm } from "../../shared/hooks/form-hook";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import { AuthContext } from "../../shared/context/auth-context";
 import "./PlaceForm.css";
+import Auth from "../../user/pages/Auth";
 
 const NewPlace = () => {
+  const auth = useContext(AuthContext);
   const { isLoading, error, sendRequest, clearError } = useHttpClient;
   const [formState, inputHandler] = useForm(
     {
@@ -32,17 +34,20 @@ const NewPlace = () => {
     false
   );
 
-  const placeSubmitHandler = (event) => {
+  const placeSubmitHandler = async (event) => {
     event.preventDefault();
-    sendRequest(
-      "http://localhost:5000/api/places",
-      "POST",
-      JSON.stringify({
-        title: formState.inputs.title.value,
-        description: formState.inputs.description.value,
-        address: formState.inputs.address.value,
-      })
-    );
+    try {
+      await sendRequest(
+        "http://localhost:5000/api/places",
+        "POST",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+          address: formState.inputs.address.value,
+          creator: auth.userId,
+        })
+      );
+    } catch (err) {}
   };
 
   return (
