d40533f3bb312e6e7fe864df8c95cccac99f79c0
Osiris <potterosiris@gmail.com>
2020-11-21 18:33:45 -0500

    import error modal

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const UserPlaces = () => {
