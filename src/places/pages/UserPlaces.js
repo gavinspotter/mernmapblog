019365f0d1015237bf3d427a6072005395f70216
Osiris <potterosiris@gmail.com>
2020-11-21 18:25:22 -0500

    import http hook

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const UserPlaces = () => {
   const userId = useParams().userId;
