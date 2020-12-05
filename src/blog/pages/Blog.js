7994e8b9437593e06fb5f0a3e2e591725a12669e
Osiris <potterosiris@gmail.com>
2020-12-05 15:24:34 -0500

    use state

--- src/blog/pages/Blog.js
+++ src/blog/pages/Blog.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import BlogList from "../components/BlogList";
 
 const Blog = () => {
 
+  const [loadedEntrys, setLoadedEntrys] = useState()
   const userId = useParams().userId;
   
+
   // return <BlogList items={loadedEntrys} />;
 };
 
