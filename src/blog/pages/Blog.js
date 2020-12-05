29e1f5984db6b22e77ad20949924caeddf53f389
Osiris <potterosiris@gmail.com>
2020-12-05 15:25:23 -0500

    import http hook

--- src/blog/pages/Blog.js
+++ src/blog/pages/Blog.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import BlogList from "../components/BlogList";
 
+import {useHttpClient} from "../../shared/hooks/http-hook"
+
 const Blog = () => {
 
   const [loadedEntrys, setLoadedEntrys] = useState()
