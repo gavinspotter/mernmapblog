26cd6a970ef585a4a3d9f62c5122e0a638d33bc8
Osiris <potterosiris@gmail.com>
2020-12-07 16:06:34 -0500

    pick img handler

--- src/shared/components/FormElements/ImageUpload.js
+++ src/shared/components/FormElements/ImageUpload.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import Button from "./Button"
+
 import './ImageUpload.css'
 
 const ImageUpload = props => {
 
+    const pickImageHandler = () => {}
+
+    return (
+        <div className="form-control">
+            <input id={props.id} style={{display: "none"}} type="file" accept=".jpg,.png,.jpeg"/>
+            <div className={`image-upload ${props.center && "center"}`}>
+            <div className="image-upload__preview">
+                <img src="" alt="preview"/>
+            </div>
+            <Button type="button" onClick={}> pick image</Button>
+            </div>
+
+        </div>
+    )
 }
 
 export default ImageUpload
