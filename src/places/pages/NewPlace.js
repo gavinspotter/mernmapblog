7f867278202d1b999ab95ec6874a47e2cdf4460a
Osiris <potterosiris@gmail.com>
2020-12-07 18:09:00 -0500

    img upload comp

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -5,6 +5,7 @@ import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload"
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
