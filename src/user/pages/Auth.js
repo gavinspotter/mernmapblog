dfb1457a7d01d2cb06fcfba72341cb26f0db90a8
Osiris <potterosiris@gmail.com>
2020-12-07 16:54:03 -0500

    post method fixed

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -71,6 +71,8 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
+    console.log(formState.inputs)
+
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
